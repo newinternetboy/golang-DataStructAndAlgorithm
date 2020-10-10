86cfc33e466302ee3ad3a73b5ee0843504e6aba8
caoxiang <caoxiang@soyoung.com>
2020-10-10 11:50:02 +0800

    bubble sort

--- Sort/BubbleSort.go
+++ Sort/BubbleSort.go
@@ -1,22 +1,21 @@
 package Sort
 
-func BubbleSort(params []int, order int) []int {
+func BubbleSort(params []int, order int) ([]int,int) {
 	length := len(params)
 	if length <= 1 {
-		return params
+		return params,0
 	}
 
-	swap := 1
-	for swap > 0 {
-		//只要交换了就要重新做一次全局位置交换
-		swap = 0
+	swap := 0
+	//总共遍历次数为数组的长度减一
+	for circle := 0; circle < len(params); circle++ {
+		//每次数组的遍历都会将最小的或者最大的置顶
 		for itemIndex := 0; itemIndex < length-1; itemIndex++ {
 			//降序
-			if order ==0 && params[itemIndex] < params[itemIndex+1] {
+			if order == 0 && params[itemIndex] < params[itemIndex+1] {
 				params[itemIndex], params[itemIndex+1] = params[itemIndex+1], params[itemIndex]
 				swap += 1
 			}
-
 			//升序
 			if order == 1 && params[itemIndex] > params[itemIndex+1] {
 				params[itemIndex], params[itemIndex+1] = params[itemIndex+1], params[itemIndex]
@@ -24,5 +23,6 @@ func BubbleSort(params []int, order int) []int {
 			}
 		}
 	}
-	return params
+
+	return params, swap
 }
