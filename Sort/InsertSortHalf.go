d47f399b809e87d439a9b1d24b18a6996cbf0847
caoxiang <caoxiang@soyoung.com>
2020-10-16 14:57:23 +0800

    impression

--- Sort/InsertSortHalf.go
+++ Sort/InsertSortHalf.go
@@ -28,3 +28,64 @@ func InsertSortHalf(params []int) []int {
 	}
 	return params
 }
+
+func InsertSortHalfV2(params []int) []int {
+	n := len(params)
+	if n <= 1 {
+		return params
+	}
+	for i := 1; i < n; i++ {
+		low := 0
+		tmp := params[i]
+		high := i - 1
+		for low <= high {
+			mid := low + (high-low)/2
+			if tmp < params[mid] {
+				high = mid - 1
+			} else {
+				low = mid + 1
+			}
+		}
+		//low就是插入的位置
+		//插入位置之后的元素后移
+		j := i
+		for ; j > low; j-- {
+			params[j] = params[j-1]
+		}
+
+		params[low] = tmp
+	}
+	return params
+}
+
+func InserSortHalfV3(params []int) []int {
+	n := len(params)
+	if n <= 1 {
+		return params
+	}
+	for i := 1; i < n; i++ {
+		//临时存储
+		tmp := params[i]
+		//寻找插入位置
+		low := 0
+		high := i - 1
+		for low <= high {
+			mid := low + (high-low)/2
+			if tmp < params[mid] {
+				high = mid - 1
+			} else {
+				low = mid + 1
+			}
+		}
+
+		//插入位置后面到插入关键字原位置前的元素后移
+		j := i
+		for ; j > low; j-- {
+			params[j] = params[j-1]
+		}
+
+		//插入
+		params[low] = tmp
+	}
+	return params
+}
